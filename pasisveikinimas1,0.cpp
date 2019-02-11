550c826efc4731a826cfe5c41213ebd397bca5f9
ToNyHasK <43828825+ToNyHasK@users.noreply.github.com>
2019-02-11 17:30:17 +0200

    0.1

---
+++ pasisveikinimas1,0.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+
+    string vardas;
+    cout << "Iveskite varda: " << endl;
+    cin >> vardas;
+    string sveikinimas;
+     if (vardas.back() == 's') {
+    sveikinimas = "Sveikas, " + vardas + "!";
+  } else {
+    sveikinimas = "Sveika, " + vardas + "!";
+  }
+    int eilIlgis = sveikinimas.length() + 4;
+    string eil1, eil2, eil3, eil4, eil5;
+//Pirma eilute
+    for (int i = 0; i < eilIlgis; i++) {
+        eil1 += "*";
+    }
+//Antra eilute
+    eil2 += "*";
+    for (int i = 0; i < eilIlgis-2; i++) {
+        eil2 += " ";
+    }
+    eil2 += "*";
+//Trecia eilute
+    eil3 = "* " + sveikinimas + " *";
+
+//Ketvirta eilute
+    eil4 = eil2;
+// Penkta eilute
+    eil5 = eil1;
+
+    cout << eil1 << endl << eil2 << endl << eil3 << endl << eil4 << endl << eil5;
+
+    return 0;
+}
