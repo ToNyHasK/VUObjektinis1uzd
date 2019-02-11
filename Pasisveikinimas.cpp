615401d65ce2ca4660d38f4b32cd1e3086cfea58
ToNyHasK <43828825+ToNyHasK@users.noreply.github.com>
2019-02-11 19:38:46 +0200

    Third commit

--- Pasisveikinimas.cpp
+++ Pasisveikinimas.cpp
@@ -1,40 +1,51 @@
 #include <iostream>
 #include <string>
+
+
 using namespace std;
 
 int main()
 {
+    cout << "Iveskit savo varda" << endl;
+        string vardas;
+            cin >> vardas;
 
-    string vardas;
-    cout << "Iveskite varda: " << endl;
-    cin >> vardas;
     string sveikinimas;
-     if (vardas.back() == 's') {
+
+        if (vardas.back() == 's') {
     sveikinimas = "Sveikas, " + vardas + "!";
   } else {
     sveikinimas = "Sveika, " + vardas + "!";
   }
-    int eilIlgis = sveikinimas.length() + 4;
-    string eil1, eil2, eil3, eil4, eil5;
-//Pirma eilute
-    for (int i = 0; i < eilIlgis; i++) {
-        eil1 += "*";
-    }
-//Antra eilute
-    eil2 += "*";
-    for (int i = 0; i < eilIlgis-2; i++) {
-        eil2 += " ";
-    }
-    eil2 += "*";
-//Trecia eilute
-    eil3 = "* " + sveikinimas + " *";
 
-//Ketvirta eilute
-    eil4 = eil2;
-// Penkta eilute
-    eil5 = eil1;
+        int starpai = 1;
+            cout << "Iveskite tarpus nuo 1 iki 10" << endl;
+                cin >> starpai;
 
-    cout << eil1 << endl << eil2 << endl << eil3 << endl << eil4 << endl << eil5;
+    const int eilutes = starpai * 2 + 3;
+    string::size_type stulpeliai = sveikinimas.size() + starpai * 2 + 2;
+    cout << endl;
+
+    for (int a =0; a!=eilutes; ++a)
+    {
+        string::size_type b = 0;
+        while (b != stulpeliai)
+            //sveikinimo laikas
+        {
+            if (a == starpai + 1 && b == starpai + 1) {
+        cout << sveikinimas;
+        b += sveikinimas.size();
+        }
+        else {
+            if (a==0||a==eilutes -1||b==0||b==stulpeliai-1)
+                cout << "*";
+                else
+                    cout << " ";
+                ++b;
+            }
+        }
+        cout << endl;
+    }
 
     return 0;
 }
